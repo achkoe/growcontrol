10e0ef34627b70c25173efa5a58c3a71b0b19700
achimk <achkoe@online.de>
2024-10-18 16:51:10 +0200

    added log output to watchdog

--- watchdog.py
+++ watchdog.py
@@ -1,17 +1,22 @@
 import os
 import pathlib
 import time
+import datetime
+from collections import deque
 import subprocess
 
 
 processes = {
-    0: dict(folder="http", args=['flask', 'run', "--host", "0.0.0.0"], t=0),
-    1: dict(folder=".", args=['python', 'servers/sensors_server.py'], t=0), 
-    2: dict(folder=".", args=['python', 'servers/light_server.py'], t=0), 
-    3: dict(folder=".", args=['python', 'servers/fan_server.py'], t=2), 
-    4: dict(folder=".", args=['python', 'servers/pump_server.py', '1'], t=0), 
-    5: dict(folder=".", args=['python', 'servers/logdata_server.py'], t=2), 
+    0: dict(name="http_server", folder="http", args=['flask', 'run', "--host", "0.0.0.0"], t=0),
+    1: dict(name="sensor_server", folder=".", args=['python', 'servers/sensors_server.py'], t=0), 
+    2: dict(name="light_server", folder=".", args=['python', 'servers/light_server.py'], t=0), 
+    3: dict(name="fan_server", folder=".", args=['python', 'servers/fan_server.py'], t=2), 
+    4: dict(name="pump_server_1", folder=".", args=['python', 'servers/pump_server.py', '1'], t=0), 
+    5: dict(name="logdata_server", folder=".", args=['python', 'servers/logdata_server.py'], t=2), 
 }
+
+logfilename = "watchdog.log"
+logqueue = deque(maxlen=16)
 
 def start():
     for key in [1, 2, 3, 4, 5]:
@@ -40,9 +45,16 @@ def watch():
             status = processes[key]["p"].poll()
             print("{}: {}".format(key, status), end=" ")
             if status is not None:
+                logqueue.append("{0}: {1} has return code {2}".format(
+                    datetime.datetime.now().isoformat(),
+                    processes[key]["name"], 
+                    status))
                 # one process has terminated, so kill all started processes and call start again
+                with pathlib.Path.cwd().joinpath(logfilename).open("w") as fh:
+                    for line in logqueue:
+                        print(line, file=fh)
+                        print(line)
                 print()
-                print("{} has return code {}".format(" ".join(processes[key]["args"]), status))
                 for killkey in processes:
                     if killkey == key:
                         continue
@@ -53,6 +65,9 @@ def watch():
         
 
 if __name__ == "__main__":
+    with pathlib.Path.cwd().joinpath(logfilename).open("w") as fh:
+        # create log file
+        pass
     start()
     watch()
     
