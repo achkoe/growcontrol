6a4f370032992ec6512e2b5be980e51d0fdddaa8
achimk <achkoe@online.de>
2024-11-28 07:10:46 +0100

    merged changes from v1-2

--- servers/fan_server.py
+++ servers/fan_server.py
@@ -59,37 +59,26 @@ class Bridge():
     def _execute(self):
         LOGGER.debug(f"state={self.state}")
         temperature = float(self.sensors_proxy.temperature())
-        humidity = float(self.sensors_proxy.humidity())
 
         def ts(time_struct): return time_struct.tm_min
 
         if self.fan_mode_manual is False:
-            if temperature > float(self.settings["temperature_high_level"]) or humidity > float(self.settings["humidity_high_level"]):
-                # start fan to reduce temperature or humidity
-                self.fan_is_on = True
-                self.state = DOWN
-                LOGGER.info(f"1:state: DOWN")
-            elif self.state == DOWN and temperature < float(self.settings["temperature_low_level"]) and humidity < float(self.settings["humidity_low_level"]):
-                self.fan_is_on = False
-                self.state = WAIT
-                LOGGER.info(f"2:state: WAIT")
-            else:
-                time_struct = time.localtime()
-                # LOGGER.critical(f"state -> {self.state}, tm_min -> {time_struct.tm_sec}, START_AT_MINUTE -> {START_AT_MINUTE}")
-                if self.state == WAIT:
-                    if ts(time_struct) >= START_AT_MINUTE and ts(time_struct) < START_AT_MINUTE + int(self.settings["fan_minutes_in_hour"]):
-                        # start fan if minutes is between START_AT_MINUTE and START_AT_MINUTE + fan_minutes_in_hour 
-                        LOGGER.info(f"3: state WAIT -> RUN")
-                        self.fan_is_on = True
-                        self.state = RUN
-                    else:
-                        self.fan_is_on = False
+            time_struct = time.localtime()
+            # LOGGER.critical(f"state -> {self.state}, tm_min -> {time_struct.tm_sec}, START_AT_MINUTE -> {START_AT_MINUTE}")
+            if self.state == WAIT:
+                if ts(time_struct) >= START_AT_MINUTE and ts(time_struct) < START_AT_MINUTE + int(self.settings["fan_minutes_in_hour"]):
+                    # start fan if minutes is between START_AT_MINUTE and START_AT_MINUTE + fan_minutes_in_hour 
+                    LOGGER.info(f"3: state WAIT -> RUN")
+                    self.fan_is_on = True
+                    self.state = RUN
                 else:
-                    if ts(time_struct) == START_AT_MINUTE + int(self.settings["fan_minutes_in_hour"]):
-                        # stop fan every hour + START_AT_MINUTE minutes + fan_minutes_in_hour
-                        self.fan_is_on = False
-                        self.state = WAIT
-                        LOGGER.info(f"4:state: RUN -> WAIT")
+                    self.fan_is_on = False
+            else:
+                if ts(time_struct) == START_AT_MINUTE + int(self.settings["fan_minutes_in_hour"]):
+                    # stop fan every hour + START_AT_MINUTE minutes + fan_minutes_in_hour
+                    self.fan_is_on = False
+                    self.state = WAIT
+                    LOGGER.info(f"4:state: RUN -> WAIT")
         else:
             self.fan_is_on = self.fan_on
         if self.fan_is_on != self.previous_fan_is_on:
