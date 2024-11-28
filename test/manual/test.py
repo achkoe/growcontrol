67e602bbf67f0b6824945008ad5945901bac1f72
achimk <achkoe@online.de>
2024-11-28 17:42:38 +0100

    added color output, renamed some files

--- test/manual/test.py
+++ test/manual/test.py
@@ -10,6 +10,7 @@ import pathlib
 import xmlrpc.client
 import re
 import pytest
+from colorama import Fore, Back, Style, init
 import test.tst_configuration as tst_configuration
 import configuration
 from servers.base import load_settings
@@ -17,6 +18,8 @@ from servers.fan_server import Bridge, START_AT_MINUTE
 
 
 LOGGER = logging.getLogger()
+init(autoreset=True)
+COLOR = Back.MAGENTA + Fore.WHITE
     
     
 @pytest.fixture
@@ -55,62 +58,76 @@ def read_last_output():
 def test_temperature(mock_proxy):
     LOGGER.info("set temperature to 32")
     mock_proxy.set_temperature(32)
-    input("Verify that 'Temperature' is red")
+    print(COLOR + "Verify that 'Temperature' is red", end=" ")
+    input("Press key to continue")
     LOGGER.info("set temperature to 22")
     mock_proxy.set_temperature(22)
-    input("Verify that 'Temperature' is green")
+    print(COLOR + "Verify that 'Temperature' is green", end=" ")
+    input("Press key to continue")
 
 
 def test_humidity(mock_proxy):
     LOGGER.info("set humidity to 32")
     mock_proxy.set_humidity(32)
-    input("Verify that 'Humidity' is red")
+    print(COLOR + "Verify that 'Humidity' is red", end=" ")
+    input("Press key to continue")
     LOGGER.info("set humidity to 22")
     mock_proxy.set_humidity(65)
-    input("Verify that 'Humidity' is green")
+    print(COLOR + "Verify that 'Humidity' is green", end=" ")
+    input("Press key to continue")
         
 
 def test_pump_manual_auto(pump_proxy):
     LOGGER.info(f"set pump Manual On")
     pump_proxy.set("Manual", "On")
-    input("Verify that pump is in state 'Manual' and 'On")
+    print(COLOR + "Verify that pump is in state 'Manual' and 'On", end=" ")
+    input("Press key to continue")
     LOGGER.info(f"set pump Manual Off")
     pump_proxy.set("Manual", "Off")
-    input("Verify that pump is in state 'Manual' and 'Off")
+    print(COLOR + "Verify that pump is in state 'Manual' and 'Off", end=" ")
+    input("Press key to continue")
     LOGGER.info(f"set pump Auto Off")
     pump_proxy.set("Auto", "Off")
-    input("Verify that pump is in state 'Auto' and 'Off' and 'WAIT'")
+    print(COLOR + "Verify that pump is in state 'Auto' and 'Off' and 'WAIT'", end=" ")
+    input("Press key to continue")
     LOGGER.info(f"set pump Auto On")
     pump_proxy.set("Auto", "On")
-    input("Verify that pump is in state 'Auto' and 'On' and 'WAIT'")
+    print(COLOR + "Verify that pump is in state 'Auto' and 'On' and 'WAIT'", end=" ")
+    input("Press key to continue")
     
     
 def test_pump_auto(mock_proxy):
     LOGGER.info("set moisture to min  (16000)")
     mock_proxy.set_value(16000)
-    input("Verify that pump is 'RUN'")
+    print(COLOR + "Verify that pump is 'RUN'", end=" ")
+    input("Press key to continue")
     LOGGER.info("set moisture to max (7600)")
     mock_proxy.set_value(7600)
-    input("Verify that pump is 'Off'")
+    print(COLOR + "Verify that pump is 'Off'", end=" ")
+    input("Press key to continue")
     
 
 def test_fan(fan_proxy, caplog):
-    input("Press button Fan/Auto to put the fan in mode 'Manual'")
+    print(COLOR + "Press button Fan/Auto to put the fan in mode 'Manual'", end=" ")
+    input("Press key to continue")
     r = fan_proxy.get_fan()
     e = "OFF"
     show(e, r)
     r = fan_proxy.get_fan_mode()
     e = "Manual"
     show(e, r)
-    input("Press button Fan/Auto/Off to put the fan in state 'On'")
+    print(COLOR + "Press button Fan/Auto/Off to put the fan in state 'On'", end=" ")
+    input("Press key to continue")
     r = fan_proxy.get_fan()
     e = "ON"
     show(e, r)
-    input("Press button Fan/Auto/On to put the fan in state 'Off")
+    print(COLOR + "Press button Fan/Auto/On to put the fan in state 'Off", end=" ")
+    input("Press key to continue")
     r = fan_proxy.get_fan()
     e = "OFF"
     show(e, r)
-    input("Press button Fan/Auto to put the fan in mode 'Auto'")
+    print(COLOR + "Press button Fan/Auto to put the fan in mode 'Auto'", end=" ")
+    input("Press key to continue")
     r = fan_proxy.get_fan_mode()
     e = "Auto"
     show(e, r)
@@ -120,10 +137,12 @@ def test_fan(fan_proxy, caplog):
         
 def test_light(light_proxy, caplog):
     ee = light_proxy.get()
-    input("Press button Light/Auto to put the light in mode 'Manual'")
+    print(COLOR + "Press button Light/Auto to put the light in mode 'Manual'", end=" ")
+    input("Press key to continue")
     r = light_proxy.get()
     show(ee, r)
-    input("Press button Light/On/Off to toggle the light")
+    print(COLOR + "Press button Light/On/Off to toggle the light", end=" ")
+    input("Press key to continue")
     e = {"light": "OFF" if r["light"] == "ON" else "OFF"}
     r = light_proxy.get()
     show(e, r)
@@ -134,7 +153,8 @@ def test_light(light_proxy, caplog):
     assert pin == configuration.port_light
     assert state == e
     
-    input("Press button Light/On/Off to toggle the light")
+    print(COLOR + "Press button Light/On/Off to toggle the light", end=" ")
+    input("Press key to continue")
     e = {"light": "ON" if r["light"] == "OFF" else "ON"}
     r = light_proxy.get()
     show(e, r)
@@ -145,7 +165,8 @@ def test_light(light_proxy, caplog):
     assert pin == configuration.port_light
     assert state == e
     
-    input("Press button Light/Auto to put the light in mode 'Auto'")
+    print(COLOR + "Press button Light/Auto to put the light in mode 'Auto'", end=" ")
+    input("Press key to continue")
     r = light_proxy.get()
     show(ee, r)
     for record in caplog.records:
