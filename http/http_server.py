4907cd0940be9048cbc1eb0834aada124c02f5aa
achimk <achkoe@online.de>
2026-04-19 16:22:31 +0200

    saved work

--- http/http_server.py
+++ http/http_server.py
@@ -2,118 +2,34 @@
 
 import xmlrpc.client
 import time
+import json
 import logging
 import pathlib
 from flask import Flask, render_template, request, redirect, url_for
 from icecream import ic
-import configuration
 from version import VERSION                                                                                                                                                                                                                                                         
 from servers.base import load_settings, save_settings
 
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
-sensors_proxy = xmlrpc.client.ServerProxy(
-    f"http://localhost:{configuration.sensors_server_port}")
-fan_proxy = xmlrpc.client.ServerProxy(
-    f"http://localhost:{configuration.fan_server_port}")
-light_proxy = xmlrpc.client.ServerProxy(
-    f"http://localhost:{configuration.light_server_port}")
-pump_proxy = xmlrpc.client.ServerProxy(
-    f"http://localhost:{configuration.pump_server_port}")
-logdata_proxy = xmlrpc.client.ServerProxy(
-    f"http://localhost:{configuration.logdata_server_port}")
-
-
-settings = load_settings()
-print(f"settings -> {settings}")
-
-mode_dict = {
-    'fan-mode': light_proxy.get_mode(),
-    'heater-mode': light_proxy.get_mode(),
-    'humidifier-mode': light_proxy.get_mode(),
-    'light-mode': light_proxy.get_mode(),
-    'pump-mode': pump_proxy.get_mode(),
-}
-
-onoff_dict = {
-    'light-onoff': light_proxy.get(),
-    'fan-onoff': fan_proxy.get_fan(),
-    'exhaustfan-onoff': fan_proxy.get_fan_exhaust_air(),
-    "heater-onoff": fan_proxy.get_heater(), 
-    "humidifier-onoff": fan_proxy.get_humidifier(),
-    "exhaustfan-onoff": fan_proxy.get_fan_exhaust_air(),
-    "pump-onoff": pump_proxy.get(),
-}
-
-onoff_set_dict = {
-    'light-onoff': light_proxy.set,
-    'fan-onoff': fan_proxy.set_fan,
-    'exhaustfan-onoff': fan_proxy.set_fan_exhaust_air,
-    "heater-onoff": fan_proxy.set_heater, 
-    "humidifier-onoff": fan_proxy.set_humidifier,
-    "exhaustfan-onoff": fan_proxy.set_fan_exhaust_air,
-    "pump-onoff": pump_proxy.set,
-}
 
 app = Flask(__name__)
-
+configuration = dict(moisture_dict=dict())
 
 @ app.route("/")
 def index():
     return render_template('index.html', 
-                           configuration=configuration, 
                            version=f"v{VERSION}",
+                           configuration=configuration,
                            settings=load_settings(raw=True))
 
 
 @ app.route("/status")
 def status():
-    humidity = sensors_proxy.humidity()
-    temperature = sensors_proxy.temperature()
-    waterlevel = sensors_proxy.waterlevel()
-    humidifier = fan_proxy.get_humidifier()
-    humidifier_mode = fan_proxy.get_humidifier_mode()
-    fan = fan_proxy.get_fan()
-    fan_mode = fan_proxy.get_fan_mode()
-    heater = fan_proxy.get_heater()
-    heater_mode = fan_proxy.get_heater_mode()
-    fan_exhaust_air = fan_proxy.get_fan_exhaust_air()
-    light = light_proxy.get()
-    light_mode = light_proxy.get_mode()
-    pump = pump_proxy.get()
-    pump_mode = pump_proxy.get_mode()
-    moisture = dict((key, sensors_proxy.moisture(configuration.moisture_dict[key]["channel"]))
-                    for key in configuration.moisture_dict)
-    
-    reply = {
-        "value-humidity": humidity,
-        "value-temperature": temperature,
-        "value-time": time.strftime("%X"),
-        "value-watersupplylevel": waterlevel,
-        "value-fan": fan,
-        "value-humidifier": humidifier,
-        "value-heater": heater,
-        "value-light": light,
-        "value-pump": pump,
-        #
-        "light-mode": light_mode,
-        "pump-mode": pump_mode,
-        "fan-mode": fan_mode,
-        "heater-mode": heater_mode,
-        "humidifier-mode": humidifier_mode,
-        #
-        "exhaustfan-onoff": fan_exhaust_air,
-        "light-onoff": light,
-        "pump-onoff": pump,
-        "fan-onoff": fan, 
-        "heater-onoff": heater, 
-        "humidifier-onoff": humidifier,
-    }
-    for key in configuration.moisture_dict.keys():
-        reply.update({f"soilmoisture-{key}": moisture[key]})
-    reply.update({"settings": settings})
+    with pathlib.Path(__file__).parent.joinpath("_data.json").open("r") as fh:
+        data = json.load(fh)
+    reply = data
     return reply
 
 
--- http/static/js/script.js
+++ http/static/js/script.js
@@ -5,153 +5,33 @@ let ids = [];
 
 // Polling function
 function pollStatus() {
+    log("pollStatus");
     fetch('/status')
-        .then(res => res.json())
+        .then(result => result.json())
         .then(data => {
-            // console.log(data);
-            for (id of [ "value-temperature", "value-humidity"]) {
-                let e = document.getElementById(id);
-                e.innerText = parseFloat(data[id]).toFixed(1);
-            }
-            for (id of ["value-time", "value-watersupplylevel", "value-fan", "value-heater", "value-light", "value-humidifier", "value-pump"]) {
-                let e = document.getElementById(id)
-                e.innerText = data[id];
-                if (["ON", "OFF"].includes(data[id])) {
-                    e.parentElement.classList.remove("on");
-                    e.parentElement.classList.remove("off");
-                    e.parentElement.classList.add(data[id].toLowerCase());
-                }
-            }
-            for (let i = 1; i < 10; i++) {
-                let key = `soilmoisture-${i}`
-                let e = document.getElementById(key);
-                if (e === null) break;
-                e.innerText = parseFloat(data[key].toFixed(1));
-                e = e.parentElement;
-                e.classList.remove("tolow");
-                e.classList.remove("acceptable");
-                if (data[key] <= data["settings"]["moisture_low_level"]) {
-                    e.classList.add("tolow");
-                    e.title = "to low";
+            document.getElementById("d-time").innerText = data.time;
+
+            for (const id of ["temperature", "humidity"]) {
+                document.getElementById(`d-${id}`).innerText = data[id];
+                let element = document.getElementById(`s-${id}`);
+                element.classList.remove("status-below", "status-above");
+                if (data[`${id}-status`] == "below") {
+                    element.classList.add("status-below");
+                    element.innerText = "below";
+                } else if (data[`${id}-status`] == "above") {
+                    element.classList.add("status-above");
+                    element.innerText = "above";
                 } else {
-                    e.classList.add("acceptable");
+                    
                 }
+                
             }
-            for (const key of ["temperature", "humidity"]) {
-                let e = document.getElementById(`value-${key}`).parentElement;
-                e.classList.remove("tohigh");
-                e.classList.remove("tolow");
-                e.classList.remove("acceptable");
-                if (data[`value-${key}`] >= data["settings"][`${key}_high_critical_level`]) {
-                    e.classList.add("tohigh");
-                    e.title = "to high"
-                } else if (data[`value-${key}`] <= data["settings"][`${key}_low_critical_level`]) {
-                    e.classList.add("tolow");
-                    e.title = "to low";
-                } else {
-                    e.classList.add("acceptable");
-                }
-            }
-            for (id of ids) {
-                var e;
-                e = document.getElementById(id);
-                e.innerText = data[id];
-                let type = id.split("-")[1];
-                if (type == "mode") {
-                    if (data[id] == "Auto") {
-                        e.classList.remove("modemanual");
-                        e.classList.add("modeauto");
-                    } else {
-                        e.classList.add("modemanual");
-                        e.classList.remove("modeauto");
-                    }
-                    let onoff = `${id.split("-")[0]}-onoff`;
-                    e = document.getElementById(onoff);
-                    if (data[id] == "Manual") {
-                        e.disabled = false;
-                    } else {
-                        e.disabled = true;
-                    }
-                } else if (type == "onoff") {
-                    e.classList.remove("on");
-                    e.classList.remove("off");
-                    e.classList.add(e.innerText.toLowerCase());
-                }
-            }
-            // waterlevel
-            {
-                const id = "value-watersupplylevel";
-                const obj = { 0: "critical", 1: "low", 2: "medium", 3: "full" }
-                let e = document.getElementById(id);
-                e.innerText = data[id] in obj ? obj[data[id]] : "unknown";
-                e = e.parentElement;
-                for (const name of ["critical", "low", "medium", "full"]) {
-                    e.classList.remove(name);
-                }
-                e.classList.add(obj[data[id]]);
-            }
+            log(data);
         });
 }
 
 window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
-
-    ids = ["fan-mode", "fan-onoff", "pump-mode", "pump-onoff", "heater-mode", "heater-onoff", "humidifier-mode", "humidifier-onoff", "light-mode", "light-onoff", "exhaustfan-onoff"];
-    for (const id of ids) {
-        document.getElementById(id).addEventListener("click", function (event) {
-            fetch(`/control`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ "id": this.id })
-            });
-        });
-    }
-
-    // tab handling
-    const tabs = document.querySelectorAll(".tab");
-    for (const tab of tabs) {
-        tab.addEventListener("click", function (event) {
-            for (let tab of tabs) {
-                tab.classList.remove("active");
-                document.getElementById(tab.getAttribute("data-tab")).style.display = "none";
-            }
-            this.classList.add("active");
-            let id = this.getAttribute("data-tab");
-            let e = document.getElementById(id).style.display = "block";
-
-            if (id == "logdata") {
-                makeLogHttpRequest('/logdata');
-                logDataIntervalTimer = setInterval(makeLogHttpRequest, 20000, '/logdata');
-            } else {
-                if (logDataIntervalTimer !== undefined) {
-                    clearInterval(logDataIntervalTimer);
-                }
-            }
-            if (id == "watchdog") {
-                fetch("/watchdog")
-                    .then(response => {
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok ' + response.statusText);
-                        }
-                        return response.json();
-                    })
-                    .then(data => {
-                        log(data);
-                        document.getElementById("watchdog-status").innerHTML = data["watchdog"];
-                    })
-                    .catch(error => {
-                        console.error('There has been a problem with your fetch operation:', error);
-                    });
-            }
-        });
-    }
-
-    tthplot = new uPlot(tthoptions, [], document.getElementById("tthgraph"));
-    for (let index = 1; ; index++) {
-        let e = document.getElementById("moisturegraph_" + index);
-        if (e === null) break;
-        moistureplot[index] = new uPlot(moistureoptions, [], e);
-    }
 
     setInterval(pollStatus, 500);
     pollStatus();
