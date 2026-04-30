019b6062fd4f6a2e6b532d8fb59fa4c331625ec4
achimk <achkoe@online.de>
2026-04-30 07:51:48 +0200

    reverted last changes

--- http/http_server.py
+++ http/http_server.py
@@ -60,7 +60,6 @@ def status():
     
     try:
         data["time"] = time.strftime("%X")
-        data["stime"] = time.time()
         data.update(sensors_proxy.get())
         
         for key in ["temperature", "humidity"]:
